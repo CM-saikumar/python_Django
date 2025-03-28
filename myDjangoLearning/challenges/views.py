06d307e479fa298718e7e5da6ffa67e5b09a1041
Sai Kumar K G <sai.kumarkg@classmethod.jp>
2025-03-29 01:01:07 +0900

    this views is means for application, like what kind of content needs to be displayed

--- myDjangoLearning/challenges/views.py
+++ myDjangoLearning/challenges/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def firstChallenge(request):
+    return HttpResponse("this is challenges page")
