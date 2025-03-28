1b0100075929967350ffd6c991f9cb1d4e406344
Sai Kumar K G <sai.kumarkg@classmethod.jp>
2025-03-29 01:33:10 +0900

    dynamic way of routing by using django placeholder < >

--- myDjangoLearning/challenges/views.py
+++ myDjangoLearning/challenges/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
-def firstChallenge(request):
-    return HttpResponse("this is challenges page")
+
+def monthlyChallenges(request, month):
+    everyMonthInformation = None
+    if month == "january":
+        everyMonthInformation = "this is january month"
+    elif month == "february":
+        everyMonthInformation = "this is february month"
+    else:
+        return HttpResponseNotFound("not found")
+    return HttpResponse(everyMonthInformation)
