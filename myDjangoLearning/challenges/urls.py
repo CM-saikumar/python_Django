62b6223bf324ca14d00918a8d0d68f463fa23035
Sai Kumar K G <sai.kumarkg@classmethod.jp>
2025-03-30 02:43:35 +0900

    added comment

--- myDjangoLearning/challenges/urls.py
+++ myDjangoLearning/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.list_of_months),
+    path("", views.list_of_months), # /challenges/
     path("<int:month>", views.monthly_challenges_by_number),
     path("<str:month>", views.monthly_challenges, name="montly-challenge")
 ]
