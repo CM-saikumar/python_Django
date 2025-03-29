be08f40f8a71c97e0aa09c46e6210eb51afa9d68
Sai Kumar K G <sai.kumarkg@classmethod.jp>
2025-03-30 02:42:57 +0900

    refactor dynamic logic instead of hardcoding the path in URL

--- myDjangoLearning/challenges/views.py
+++ myDjangoLearning/challenges/views.py
@@ -101,6 +101,18 @@ japan_travel_challenges = {
     }
 }
 
+def list_of_months(request):
+    list_items = ""
+    months = list(japan_travel_challenges.keys())
+
+    for month in months:
+        capitalize_month = month.capitalize()
+        month_path = reverse("montly-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalize_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 
 def monthly_challenges_by_number(request, month):
     months = list(japan_travel_challenges.keys())
